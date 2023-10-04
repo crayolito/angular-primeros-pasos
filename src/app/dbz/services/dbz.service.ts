cf1ffe3d12a270ffa41914de9ec659cc7eec27a1
crayolito <josalejandroapp2017@gmail.com>
2023-10-04 18:01:52 -0400

    primer commit

---
+++ src/app/dbz/services/dbz.service.ts
@@ -0,0 +1,45 @@
+import { v4 as uuid} from 'uuid';
+import { Injectable } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
+
+@Injectable({providedIn: 'root'})
+export class DbzService {
+
+  public characters: Character[] = [
+    {
+      id: uuid(),
+      name: 'Krillin',
+      power: 1000
+    },{
+      id: uuid(),
+      name: 'Goku',
+      power: 9500
+    },
+    {
+      id: uuid(),
+      name:'Vegeta',
+      power: 7500
+    }
+  ];
+
+  onDeleteCharacterID(id:string):void
+  {
+
+    // this.characters.splice(id,1);
+    this.characters = this.characters.filter(character => character.id !==id);
+
+  }
+
+  onNewCharacter(character:Character):void{
+    // const newCharacter: Character={
+    //   id: uuid(),
+    //   name: character.name,
+    //   power: character.power
+    // }
+    const newCharacter:Character ={id: uuid(), ...character};
+    this.characters.push(newCharacter);
+  }
+
+  constructor() { }
+
+}
